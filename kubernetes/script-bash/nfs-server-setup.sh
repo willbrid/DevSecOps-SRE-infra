b895110abf8ad697f89e1c4e3434d74ca03061c2
willbrid <ngaswilly77@gmail.com>
2024-04-21 09:51:24 +0100

    feat: refactor for bash convention

--- kubernetes/script-bash/nfs-server-setup.sh
+++ kubernetes/script-bash/nfs-server-setup.sh
@@ -16,15 +16,16 @@
 #   --device-size           Valeur de pourcentage de stockage de la partition primaire à créer. Valeur entre 1 et 100. Par défaut : 100
 
 # Initialisation des variables
-nfsnetwork=""
-sharedname="nfsshared"
-validationNFSnetwork=""
-deviceName=""
-deviceNum=""
-deviceSize="100"
+nfs_network=""
+device_name=""
+device_num=""
+device_size="100"
+
+readonly VALIDATION_NFS_NETWORK="^([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}$"
+readonly SHARED_NAME="nfsshared"
 
 # Vérification de l'exécution en mode root
-checkIfRoot() {
+check_if_root() {
     if [ "$EUID" -ne 0 ]; then
         echo "Ce script doit être exécuté en tant que root."
         exit 1
@@ -32,7 +33,7 @@ checkIfRoot() {
 }
 
 # Définition de la fonction d'aide
-displayHelp() {
+display_help() {
     echo "Usage: $0 [options]"
     echo "Options:"
     echo "  -h, --help              Afficher cette aide"
@@ -43,63 +44,61 @@ displayHelp() {
 }
 
 # Vérification des options entrées
-verifyOptions() {
-    if [[ -z $nfsnetwork ]]; then
+verify_options() {
+    if [[ -z $nfs_network ]]; then
         echo "L'option --nfs-network est obligatoire."
-        displayHelp
+        display_help
         exit 1
     fi
 
-    validationNFSnetwork="^([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}$"
-    if ! [[ $nfsnetwork =~ $validationNFSnetwork ]]; then
-        echo "Erreur: $nfsnetwork n'est pas sous le format cidr (ex: 192.168.56.0/24)."
+    
+    if ! [[ $nfs_network =~ $VALIDATION_NFS_NETWORK ]]; then
+        echo "Erreur: $nfs_network n'est pas sous le format cidr (ex: 192.168.56.0/24)."
         exit 1
     fi
 
-    if ! lsblk | grep -i "$deviceName"; then
-        echo "Le périphérique $deviceName n'est pas présent."
+    if ! lsblk | grep -i "$device_name"; then
+        echo "Le périphérique $device_name n'est pas présent."
         exit 1
     else
-        deviceName="/dev/$deviceName"
+        device_name="/dev/$device_name"
     fi
 
-    deviceNum=$(expr "$deviceNum" + 0)
+    device_num=$(expr "$device_num" + 0)
     if [ $? -ne 0 ]; then
         echo "Veuillez entrer un numéro de phériphérique compris entre 1 et 4."
         exit 1
     fi
-    if (( $deviceNum < 1 || $deviceNum > 4 )); then
-        echo "Le numéro $deviceNum de partition primaire n'est pas compris entre 1 et 4."
+    if (( $device_num < 1 || $device_num > 4 )); then
+        echo "Le numéro $device_num de partition primaire n'est pas compris entre 1 et 4."
         exit 1
     fi
 
-    deviceSize=$(expr "$deviceSize" + 0)
+    device_size=$(expr "$device_size" + 0)
     if [ $? -ne 0 ]; then
         echo "Veuillez entrer une valeur de pourcentage du stockage de partition primaire comprise entre 1 et 100."
         exit 1
     fi
-    if (( $deviceSize < 1 || $deviceSize > 100 )); then
-        echo "La valeur de pourcentage $deviceSize du stockage de partition primaire n'est pas comprise entre 1 et 100."
+    if (( $device_size < 1 || $device_size > 100 )); then
+        echo "La valeur de pourcentage $device_size du stockage de partition primaire n'est pas comprise entre 1 et 100."
         exit 1
     fi
 }
 
 # Configuration de l'espace de stockage NFS
-configureNFSStorage() {
-    echo -e "\nConfiguration du stockage NFS en cours...\n"
-
+configure_nfs_storage() {
     mkdir -p /data
 
-    if ! ls -l $deviceName$deviceNum &> /dev/null; then
-        parted -s -a optimal -- $deviceName mklabel gpt
-        parted -s -a optimal -- $deviceName mkpart primary ext4 0% $deviceSize"%"
-        parted -s -- $deviceName align-check optimal $deviceNum
-        mkfs.ext4 $deviceName$deviceNum
-        echo "$deviceName$deviceNum /data ext4 defaults 0 0" | tee -a /etc/fstab
+    if ! ls -l $device_name$device_num &> /dev/null; then
+        parted -s -a optimal -- $device_name mklabel gpt
+        parted -s -a optimal -- $device_name mkpart primary ext4 0% $device_size"%"
+        parted -s -- $device_name align-check optimal $device_num
+        mkfs.ext4 $device_name$device_num
+        echo "$device_name$device_num /data ext4 defaults 0 0" | tee -a /etc/fstab
         mount -a
     else
         if ! df -h | grep /data; then
-            echo "$deviceName$deviceNum /data ext4 defaults 0 0" | tee -a /etc/fstab
+            echo "$device_name$device_num /data ext4 defaults 0 0" | tee -a /etc/fstab
             mount -a
         fi
     fi
@@ -108,16 +107,14 @@ configureNFSStorage() {
 }
 
 # Configuration du service de stockage NFS
-configureNFSService() {
-    echo -e "\nConfiguration du service de stockage NFS en cours...\n"
-
+configure_nfs_service() {
     dnf -q install -y nfs-utils
 
-    mkdir -p /data/$sharedname
-    chown nobody:nobody /data/$sharedname
-    chmod 2770 /data/$sharedname
+    mkdir -p /data/$SHARED_NAME
+    chown nobody:nobody /data/$SHARED_NAME
+    chmod 2770 /data/$SHARED_NAME
 
-    echo -e "/data/$sharedname\t$nfsnetwork(rw,sync,no_subtree_check,no_root_squash)" | tee -a /etc/exports
+    echo -e "/data/$SHARED_NAME\t$nfs_network(rw,sync,no_subtree_check,no_root_squash)" | tee -a /etc/exports
     exportfs -av
     systemctl enable --now rpcbind nfs-server
 
@@ -129,8 +126,8 @@ configureNFSService() {
     echo -e "\nConfiguration du service de stockage NFS : OK\n"
 }
 
-# failTrap est exécuté si une erreur se produit.
-failTrap() {
+# fail_trap est exécuté si une erreur se produit.
+fail_trap() {
     local result=$?
     
     if [ "$result" != "0" ]; then
@@ -144,34 +141,34 @@ failTrap() {
 # Execution
 
 # Arrêter l'exécution en cas d'erreur
-trap "failTrap" EXIT
+trap "fail_trap" EXIT
 set -e
 
-checkIfRoot
+check_if_root
 
 # Traitement des options avec getopts
 while getopts ":h-:" opt; do
     case ${opt} in
         h)
-          displayHelp
+          display_help
           exit 0
           ;;
         -)
             case "${OPTARG}" in
                 nfs-network=*)
-                  nfsnetwork="${OPTARG#*=}"
+                  nfs_network="${OPTARG#*=}"
                   ;;
                 device-name=*)
-                  deviceName="${OPTARG#*=}"
+                  device_name="${OPTARG#*=}"
                   ;;
                 device-num=*)
-                  deviceNum="${OPTARG#*=}"
+                  device_num="${OPTARG#*=}"
                   ;;
                 device-size=*)
-                  deviceSize="${OPTARG#*=}"
+                  device_size="${OPTARG#*=}"
                   ;;
                 help)
-                  displayHelp
+                  display_help
                   exit 0
                   ;;
                 *)
@@ -188,8 +185,8 @@ while getopts ":h-:" opt; do
 done
 shift $((OPTIND -1))
 
-verifyOptions
-configureNFSStorage
-configureNFSService
+verify_options
+configure_nfs_storage
+configure_nfs_service
 
 echo -e "\nConfiguration du serveur NFS : OK\n"
