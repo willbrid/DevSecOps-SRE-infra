eaf82e531e70735be9fb01f3b932a4a026500551
willbrid <ngaswilly77@gmail.com>
2024-04-21 11:50:39 +0100

    feat: complete haproxy installation and base configuration

--- kubernetes/script-bash/haproxy-setup.sh
+++ kubernetes/script-bash/haproxy-setup.sh
@@ -16,7 +16,6 @@
 # Initialisation des variables
 default_version="2.6.17"
 version="$default_version"
-download_url=""
 
 readonly VERSION_VALIDATION="^2\.[6-9]\.[0-50]+$"
 readonly HAS_CURL="$(type "curl" &> /dev/null && echo true || echo false)"
@@ -63,7 +62,8 @@ setup_prerequisites() {
 
 install_haproxy() {
     readonly MAJOR_VERSION="${version%.*}"
-    readonly HAPROXY_DIST="haproxy-$version.tar.gz"
+    readonly HAPROXY_DIST_NAME="haproxy-$version"
+    readonly HAPROXY_DIST="$HAPROXY_DIST_NAME.tar.gz"
     readonly HAPROXY_TMP_ROOT="$(mktemp -dt haproxy-installer-XXXXXXX)"
     readonly DOWNLOAD_URL="https://www.haproxy.org/download/$MAJOR_VERSION/src/$HAPROXY_DIST"
     
@@ -73,7 +73,80 @@ install_haproxy() {
         wget -q -O "$HAPROXY_TMP_ROOT/$HAPROXY_DIST" "$DOWNLOAD_URL"
     fi
 
+    tar Czxvf $HAPROXY_TMP_ROOT $HAPROXY_TMP_ROOT/$HAPROXY_DIST > /dev/null
+    make -C $HAPROXY_TMP_ROOT/$HAPROXY_DIST_NAME TARGET=linux-glibc USE_LUA=1 USE_OPENSSL=1 USE_PCRE=1 USE_ZLIB=1 USE_SYSTEMD=1 USE_PROMEX=1 > /dev/null
+    make -C $HAPROXY_TMP_ROOT/$HAPROXY_DIST_NAME install-bin > /dev/null
+
+    if ! command -v /usr/local/sbin/haproxy &> /dev/null; then
+        exit 1
+    fi
+
     echo -e "\nInstallation de haproxy : OK\n"
+}
+
+configure_haproxy() {
+    readonly BIGEST_UID=$(awk -F: '$3 ~ /^[0-9]{4}$/ {print $3}' /etc/passwd | sort -n | tail -n 1)
+    readonly BIGEST_GID=$(awk -F: '$4 ~ /^[0-9]{4}$/ {print $4}' /etc/passwd | sort -n | tail -n 1)
+    readonly HAPROXY_UID=$(expr "$BIGEST_UID" + 1)
+    readonly HAPROXY_GID=$(expr "$BIGEST_GID" + 1)
+
+    readonly HAPROXY_HOME="/var/lib/haproxy"
+    readonly HAPROXY_CONFIG_HOME="/etc/haproxy"
+    readonly HAPROXY_CONFIG_FILE="$HAPROXY_CONFIG_HOME/haproxy.cfg"
+    readonly HAPROXY_SYSTEMD_FILE="/etc/systemd/system/haproxy.service"
+
+    # Créer un utilisateur haproxy
+    groupadd -g $HAPROXY_GID haproxy
+    useradd -g $HAPROXY_GID -u $HAPROXY_UID -m -d $HAPROXY_HOME -s /sbin/nologin -c haproxy haproxy
+
+    mkdir -p $HAPROXY_CONFIG_HOME
+
+    cat << EOF | tee $HAPROXY_CONFIG_FILE
+global
+  zero-warning
+  maxconn 2000
+  chroot $HAPROXY_HOME
+  user haproxy
+  group haproxy
+  daemon
+  hard-stop-after 5m
+  log stderr local0 info
+
+defaults tcp
+  mode tcp
+  option tcplog
+  log global
+  timeout client 30s
+  timeout server 30s
+  timeout connect 10s
+EOF
+
+    chown -R haproxy:haproxy $HAPROXY_CONFIG_HOME
+
+    cat << EOF | tee $HAPROXY_SYSTEMD_FILE
+[Unit]
+Description=HAProxy $version
+After=syslog.target network.target
+
+[Service]
+Type=notify
+ExecStart=/usr/local/sbin/haproxy -f $HAPROXY_CONFIG_FILE -p /var/run/haproxy.pid -Ws
+ExecReload=/bin/kill -USR2 \$MAINPID
+ExecStop=/bin/kill -USR1 \$MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    setsebool -P haproxy_connect_any 1
+    systemctl daemon-reload
+    systemctl enable --now haproxy
+
+    if ! systemctl is-active --quiet haproxy; then
+        exit 1
+    fi
+
+    echo -e "\nConfiguration de base de haproxy : OK\n"
 }
 
 # Nettoyage du répertoire temporaire d'installation de haproxy
@@ -138,6 +211,7 @@ verify_options
 check_dependency
 setup_prerequisites
 install_haproxy
+configure_haproxy
 cleanup
 
 exit 0
